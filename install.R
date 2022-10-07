59cadef3bb1099f6894e79ea7ff27d1cdb08eaf3
ccbaumler <63077899+ccbaumler@users.noreply.github.com>
2022-10-07 14:48:27 -0700

    trying one more thing before deleting

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 install.packages("quanteda.textstats")
 install.packages("dplyr")
 install.packages("gt")
-install.packages("bibliometrix", repos="https://cran.rstudio.com")
+install.packages('bibliometrix', type = 'source')
