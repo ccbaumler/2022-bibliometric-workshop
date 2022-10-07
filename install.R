e383cd72a687282e978aee493b1024ab480d8b0d
ccbaumler <63077899+ccbaumler@users.noreply.github.com>
2022-10-07 13:39:38 -0700

    repos command

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
-install.packages("bibliometrix")
+install.packages("bibliometrix", repos="https://cran.rstudio.com")
 install.packages("quanteda.textstats")
 install.packages("dplyr")
 install.packages("gt")
